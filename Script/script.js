b7fb0f2cf3ba937a4cf17a5cbbf8855536dcc1fc
Levi <levirs999@gmail.com>
2025-10-12 09:19:21 -0300

    estrutura Html e Css

---
+++ Script/script.js
@@ -0,0 +1,7 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".aswers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
