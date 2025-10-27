b54afe44f9fabaaada4c95ae866e45e16c410065
Levi <levirs999@gmail.com>
2025-10-27 20:57:45 -0300

    subindo codigo atual do projeto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Projeto-Octa-Mind
+
+Projeto feito por Levi Ribeiro e Leonardo Barbosa
+
+- ## Feito em Html/CSS/Javascript
--- Script/script.js
+++ Script/script.js
@@ -1,65 +1,193 @@
-const question = document.querySelector(".question");
-const answers = document.querySelector(".answers");
-const spnQtd = document.querySelector(".spnQtd");
-const textFinish = document.querySelector(".finish span");
-const content = document.querySelector(".content");
-const contentFinish = document.querySelector(".finish");
-const btnRestart = document.querySelector(".finish button");
+const cardBox = document.querySelector('div.card-content-box')
+const btnRotateCard = document.querySelector('#rotate-card')
+const btnSubmit = document.querySelector('#input-submit')
 
-import questions from "./questions.js";
 
-let currentIndex = 0;
-let questionsCorrect = 0;
+const inputNumber = document.querySelector('#input-number')
+const inputNumberInfo = document.querySelector('#input-number + .info')
+const inputName = document.querySelector('#input-name')
+const inputNameInfo = document.querySelector('#input-name + .info')
+const inputCvv = document.querySelector('#input-cvv')
+const inputCvvInfo = document.querySelector('#input-cvv + .info')
+const inputValidate = document.querySelector('#input-validate')
+const inputValidateInfo = document.querySelector('#input-validate + .info')
 
-btnRestart.onclick = () => {
-  content.style.display = "flex";
-  contentFinish.style.display = "none";
+const cardViewName = document.querySelector('#card-user-name');
+const cardViewNumber = document.querySelector('#card-user-number');
+const cardViewCvv = document.querySelector('#card-user-cvv');
+const cardViewDate = document.querySelector('#card-user-date');
 
-  currentIndex = 0;
-  questionsCorrect = 0;
-  loadQuestion();
-};
 
-function nextQuestion(e) {
-  if (e.target.getAttribute("data-correct") === "true") {
-    questionsCorrect++;
-  }
+inputNumber.onblur = (e) => {
+	const value = e.target.value;
+	const valueReplace = value.replaceAll(' ', '')
 
-  if (currentIndex < questions.length - 1) {
-    currentIndex++;
-    loadQuestion();
-  } else {
-    finish();
-  }
+
+	if(value.length <= 0){
+		const message = "Preenchimento obrigatório!"
+		inputNumberInfo.querySelector('.message').innerText = message
+		inputNumberInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
+
+	if(!/^[0-9]{16}$/.test(valueReplace)){
+		const message = "Use apenas números, e verifique se estão completos!"
+		inputNumberInfo.querySelector('.message').innerText = message
+		inputNumberInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
+
+	inputNumberInfo.querySelector('.message').innerText = ''
+	inputNumberInfo.classList.remove('visible')
+
+	canSubmit();
 }
 
-function finish() {
-  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
-  content.style.display = "none";
-  contentFinish.style.display = "flex";
+inputName.onblur = (e) => {
+	const value = e.target.value;
+	const valueReplace = value.replaceAll(' ', '')
+
+
+	if(value.length <= 0){
+		const message = "Preenchimento obrigatório!"
+		inputNameInfo.querySelector('.message').innerText = message
+		inputNameInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
+
+	if(!/^[a-z]+$/i.test(valueReplace)){
+		const message = "Insira seu nome de forma correcta!"
+		inputNameInfo.querySelector('.message').innerText = message
+		inputNameInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
+
+	inputNameInfo.querySelector('.message').innerText = ''
+	inputNameInfo.classList.remove('visible')
+	canSubmit();
 }
 
-function loadQuestion() {
-  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
-  const item = questions[currentIndex];
-  answers.innerHTML = "";
-  question.innerHTML = item.question;
+inputValidate.onblur = (e) => {
+	const value = e.target.value;
+	const valueReplace = value.replaceAll(' ', '')
 
-  item.answers.forEach((answer) => {
-    const div = document.createElement("div");
 
-    div.innerHTML = `
-    <button class="answer" data-correct="${answer.correct}">
-      ${answer.option}
-    </button>
-    `;
+	if(value.length <= 0){
+		const message = "Preenchimento obrigatório!"
+		inputValidateInfo.querySelector('.message').innerText = message
+		inputValidateInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
 
-    answers.appendChild(div);
-  });
+	if(!/^[0-9]{2}\/[0-9]{4}/i.test(valueReplace)){
+		const message = `Use o padrão "mês/ano"`
+		inputValidateInfo.querySelector('.message').innerText = message
+		inputValidateInfo.classList.add('visible')
+		btnSubmit.classList.add('disable')
+		return false;
+	}
 
-  document.querySelectorAll(".answer").forEach((item) => {
-    item.addEventListener("click", nextQuestion);
-  });
+	inputValidateInfo.querySelector('.message').innerText = ''
+	inputValidateInfo.classList.remove('visible')
+	canSubmit();
 }
 
-loadQuestion();
+btnRotateCard.addEventListener('click', (e) => {
+	cardBox.classList.toggle('rotate')
+})
+
+const handleName = (e) => {
+
+	setTimeout(() => {
+
+		const value = e.target.value
+
+		cardViewName.innerText= value
+
+	}, 100)
+	
+}
+
+const handleNumber = (e) => {
+
+	setTimeout(() => {
+
+		const value = e.target.value
+
+
+		if(value.length >= 20) {
+			return false;
+		}
+
+		if(e.key == 'Backspace') {
+			cardViewNumber.innerText = value
+			return false
+		}
+
+		if(value.length == 4 || value.length == 9 || value.length == 14) {
+
+			e.target.value += " "
+		}
+
+		cardViewNumber.innerText = value
+
+	}, 0)
+	
+}
+
+const handleCvv = (e) => {
+
+	setTimeout(() => {
+
+		const value = e.target.value
+
+
+		cardViewCvv.innerText = value
+
+	}, 0)
+	
+}
+
+const handleValidate = (e) => {
+
+	setTimeout(() => {
+
+		const value = e.target.value
+
+
+		cardViewDate.innerText = value
+
+	}, 0)
+	
+}
+
+inputCvv.onfocus = () => {
+	cardBox.classList.remove('rotate')
+}
+
+inputCvv.onblur = () => {
+	cardBox.classList.add('rotate')
+	canSubmit();
+}
+
+function canSubmit(){
+	
+	const inputs = document.querySelectorAll('input')
+
+	for(let i = 0; i < inputs.length; i++){
+
+		if(inputs[i].value.length <= 0){
+			btnSubmit.classList.add('disable')
+			return false;
+		}
+	}
+
+	btnSubmit.classList.remove('disable')
+}
+
+canSubmit()
